17d08b904980529f6e2cdcf18c61691f524ba511
abhishek pandey <abhishekpy55@gmail.com>
2025-03-17 19:00:38 +0530

    code update mini project 2

---
+++ src/test/java/com/thetestingacadmey/selenumProject2/Moto.java
@@ -0,0 +1,11 @@
+package com.thetestingacadmey.selenumProject2;
+
+public class Moto {
+//    open the url - https://katalon-demo-cura.herokuapp.com/
+//    click on the make appoinment button
+//    verify that url changes - assert
+//            time.sleep(3)
+//    enter the username, password
+//    next page verify the current url
+//    make appoinment text on the web page.
+}
